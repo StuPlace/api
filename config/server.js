b26eebe73ef634101718655062bec3f8cc2678f4
Mutlaq Aldhbuiub <matulkn@gmail.com>
2021-06-09 04:05:35 +0000

    Project init

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'c2daa8f57f92709dfa749eba02e29e6a'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'ca06e677-2066-4934-9296-e2a6d7f921d8'
+};
